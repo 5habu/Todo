94c98e6256ff497fcd0d9928a745ab4ca19fa32d
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-18 23:32:34 +0900

    commit

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var newItem: String = ""
+    @State var todoList: [String] = ["a","b"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("Add New").font(.largeTitle).padding(.leading)
+                Spacer()
+            }
+            HStack {
+                TextField("New Plan", text: $newItem)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(width: 300)
+            
+                Button(action: {
+                    self.todoList.append(self.newItem)
+                    self.newItem = ""
+                }) {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 5)
+                            .frame(width: 50, height: 30)
+                            .foregroundColor(.green)
+                        
+                        Text("add").foregroundColor(.white)
+                    }
+                }
+            }
+            
+            HStack {
+                Text("aaa").font(.largeTitle).padding(.leading)
+                Spacer()
+            }
+            
+            List(todoList, id: \.self) { item in
+                Text(item)
+            }
+        }
     }
 }
 
