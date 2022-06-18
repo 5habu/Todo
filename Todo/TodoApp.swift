7c2283fdec284613ac5e2923c7314ac9808337c6
5habu <black.widow.of.la.porte.27@gmail.com>
2022-06-18 18:30:17 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 5habu on 2022/06/18.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
